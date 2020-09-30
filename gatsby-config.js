3fd846e69a324eb83812ca96c2c086d222a5d32b
Programming Bit <jamil.owais.a@gmail.com>
2020-09-30 13:03:15 -0500

    Edited markdown and metadata files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Programming Bits',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A blog to help you learn to code.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
